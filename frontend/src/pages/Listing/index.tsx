54d25cebc55f31033e38a9ebc618abf954fd9f0a
Luiz Repiso <luizrepiso@yahoo.es>
2022-01-13 13:03:11 -0300

    Saving Corrections

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -5,24 +5,23 @@ function Listing() {
   return (
     <>
       <Pagination />
-
       <div className="container">
         <div className="row">
           <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-              <MovieCard />
-           </div> 
-           <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-              <MovieCard />
-           </div> 
-           <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-              <MovieCard />
-           </div> 
-           <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-              <MovieCard />
-           </div> 
-           <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-              <MovieCard />
-           </div> 
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
         </div>
       </div>
     </>
