b61ee6e55ed31627dfdb2370dc27cb4dfe8276e1
Luiz Repiso <luizrepiso@yahoo.es>
2022-01-14 12:22:17 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,37 +1,44 @@
 import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
-import {BASE_URL} from "utils/Requests";
-
+import { useEffect, useState } from "react";
+import { BASE_URL } from "utils/Requests";
+import { MoviePage } from "types/movie";
 
 function Listing() {
+  const [pageNumber, setPageNumber] = useState(0);
 
+  useEffect(() => {
+  
     //FORMA ERRADA
-    axios.get (`${BASE_URL}/movies?size=12&page=0`)
-    .then(response =>{
-        console.log(response.data);
-    });
+  axios.get(`${BASE_URL}/movies?size=12&page=1`).then((response) => {
+    const data = response.data as MoviePage;
+    console.log(data);
+    setPageNumber(data.number);
+  });
+},[]);
 
   return (
     <>
+      <p>{pageNumber}</p>
       <Pagination />
 
       <div className="container">
         <div className="row">
           <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard/>
+            <MovieCard />
           </div>
           <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard/>
+            <MovieCard />
           </div>
           <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard/>
+            <MovieCard />
           </div>
           <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard/>
+            <MovieCard />
           </div>
           <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard/>
+            <MovieCard />
           </div>
         </div>
       </div>
